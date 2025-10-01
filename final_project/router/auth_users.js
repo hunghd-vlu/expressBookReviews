cfa24de0e6ac3ae9cdeb853ab1542d30704079fc
hunghd-vlu <hung.hd@vlu.edu.vn>
2025-10-01 04:46:38 -0400

    Tast 1 - 6

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,6 +7,14 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    if (username) {
+        for (let user of users) {
+            if (user.username === username) return false;
+        }
+        return true;
+    } else { 
+        return false;
+    }
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,13 +7,26 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let username = req.body.username
+  let password = req.body.password
+
+  if(username && password && isValid(username)) {
+    users.push( {
+        username,
+        password
+    })
+    return res.json({message: `Customer successfully registred.`});
+  } else {
+    console.log("Name: " + username)
+    console.log("Pass: " + password)
+    return res.send(`Unable to register, username is invalid / exists. Choose a different username.`)
+  }
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.send(JSON.stringify(books, null, 4))
+  return res.json(books)
 });
 
 // Get book details based on ISBN
@@ -21,25 +34,49 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   let isbn = req.params.isbn;
   let book = books[isbn]
-  res.send(JSON.stringify(book, null, 4));
+  return res.json(book)
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let author = req.params.author
+  let result = {
+      "booksByAuthor": []
+  }
+  let keys = Object.keys(books)
+  for(let key of keys) {
+      let book = books[key]
+      if(book.author == author) {
+          result['booksByAuthor'].push(book)
+      }
+  }
+  return res.json(result)
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let title = req.params.title
+  let result = {
+    "booksByTitle": []
+  }
+  let keys = Object.keys(books)
+  for(let key of keys) {
+      let book = books[key]
+      if(book.title == title) {
+          result['booksByTitle'].push(book)
+      }
+  }
+  return res.json(result)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let isbn = req.params.isbn
+  let review = books[isbn].reviews
+  return res.json(review)
 });
 
 module.exports.general = public_users;
